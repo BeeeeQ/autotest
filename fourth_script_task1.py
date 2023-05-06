377ae484ccc09a5b73b1e3dd455aae3ea1126bed
si.kuricyn <si.kuricyn@tensor.ru>
2023-05-06 22:58:40 +0300

    Сделал все задания плюс задание со звездочкой

---
+++ fourth_script_task1.py
@@ -0,0 +1,42 @@
+# Напишите функцию which_triangle(a, b, c),
+# На вход поступают длины трёх сторон треугольника: a, b, c
+# Программа выводит какой это треугольник type_triangle: "Равносторонний", "Равнобедренный", "Обычный".
+# Либо "Не треугольник", если по переданным параметрам невозможно построить треугольник
+# Например 1, 1, 1 --> "Равносторонний"
+
+def which_triangle(a, b, c):
+    # Здесь нужно написать код
+    equilateral = 'Равносторонний'
+    isosceles = 'Равнобедренный'
+    regular = 'Обычный'
+    not_triangle = 'Не треугольник'
+    if a == b == c:
+        type_triangle = equilateral
+    elif a == b or a == c or b == c:
+        type_triangle = isosceles
+    elif a + b > c and a + c > b and b + c > a:
+        type_triangle = regular
+    else:
+        type_triangle = not_triangle
+    return type_triangle
+
+# Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
+
+
+data = [
+    (3, 3, 3),
+    (1, 2, 2),
+    (3, 4, 5),
+    (3, 2, 3),
+    (1, 2, 3)
+]
+
+test_data = [
+    "Равносторонний", "Равнобедренный", "Обычный", "Равнобедренный", "Не треугольник"
+]
+
+
+for i, d in enumerate(data):
+    assert which_triangle(*d) == test_data[i], f'С набором {d} есть ошибка, не проходит проверку'
+    print(f'Тестовый набор {d} прошёл проверку')
+print('Всё ок')
